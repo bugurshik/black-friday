1e5a22bb00836733c6a9f5430f4525cfd77ddc14
Сергей <bugurshik@yandex.ru>
2025-06-26 23:04:08 +0300

    2c

--- mongo-sharding-repl/scripts/shards-repl-init.sh
+++ mongo-sharding-repl/scripts/shards-repl-init.sh
@@ -5,22 +5,22 @@
 ###
 
 docker compose exec -T configSrv mongosh --port 27017 <<EOF
- rs.initiate(
-  {
-    _id : "config_server",
-       configsvr: true,
-    members: [
-      { _id : 0, host : "configSrv:27017" }
-    ]
-  }
-);
+rs.initiate({
+  _id: "config_server",
+  configsvr: true,
+  members: [
+    { _id: 0, host: "configSrv:27017" }
+  ]
+});
 EOF
 docker compose exec -T shard1_node1 mongosh  --port 27018 <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1_node1:27018" },
+        {_id: 0, host: "shard1_node1:27018"},
+        {_id: 1, host: "shard1_node2:27019"},
+        {_id: 2, host: "shard1_node3:27030"}
       ]
     }
 );
@@ -30,7 +30,9 @@ rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 1, host : "shard2_node1:27021" }
+        {_id: 3, host: "shard2_node1:27021"},
+        {_id: 4, host: "shard2_node2:27022"},
+        {_id: 5, host: "shard2_node3:27023"}
       ]
     }
   );
@@ -41,5 +43,6 @@ sh.addShard( "shard2/shard2_node1:27021");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 db.helloDoc.countDocuments() 
 EOF
